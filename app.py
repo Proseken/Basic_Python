d719c9d33d8a7b077b4f3dda1c5f66318d4d4b03
Proseken <66033203+Proseken@users.noreply.github.com>
2023-01-13 03:02:28 -0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,48 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+
+print("Select operation.")
+print("+.Add")
+print("-.Subtract")
+print("*.Multiply")
+print("/.Divide")
+
+while True:
+    choice = input("Enter choice(+\-\*\/): ")
+
+    if choice in ('+', '-', '*', '/'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '+':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '-':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '*':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '/':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        
+        
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    else:
+        print("Invalid Input")
